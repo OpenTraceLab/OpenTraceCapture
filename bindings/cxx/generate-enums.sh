73743bc02640c5243078b1db48a89bf776b988a2
Carl-Fredrik Sundstrom <carl-fredrik.sundstrom@ericsson.com>
2025-10-28 06:49:32 +0100

    Use wrapper script for enums generation with multiple outputs
    Meson capture only works with single output. Use shell script wrapper.

--- bindings/cxx/generate-enums.sh
+++ bindings/cxx/generate-enums.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
-# Generate enums.cpp and enums.hpp
-# Args: python3 enums.py xml_index_file
-python3 "$1" "$2" > enums.cpp
+# Wrapper to generate enums.cpp and enums.hpp
+# Args: python3 enums.py doxy_xml build_dir output_cpp output_hpp
+
+PYTHON="$1"
+SCRIPT="$2"
+DOXY_XML="$3"
+BUILD_DIR="$4"
+OUTPUT_CPP="$5"
+OUTPUT_HPP="$6"
+
+# Run enums.py and capture stdout to enums.cpp
+"$PYTHON" "$SCRIPT" "$DOXY_XML" "$BUILD_DIR" > "$OUTPUT_CPP"
+
+# Move generated enums.hpp to the output location
+mv "$BUILD_DIR/enums.hpp" "$OUTPUT_HPP"
